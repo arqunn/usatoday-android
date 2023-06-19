c332c3adfa274c0d41e8e703060ba4ffe4ba676e
arqunn <mobi.arqun@gmail.com>
2023-06-19 15:13:31 +0300

    Material toolbar has been added

--- app/src/main/java/com/arqunn/usatoday/presentation/MainActivity.kt
+++ app/src/main/java/com/arqunn/usatoday/presentation/MainActivity.kt
@@ -2,9 +2,12 @@ package com.arqunn.usatoday.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.arqunn.usatoday.R
 import com.arqunn.usatoday.databinding.ActivityMainBinding
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar)
         setContentView(binding.root)
         setupNavController()
     }
@@ -33,13 +37,22 @@ class MainActivity : AppCompatActivity() {
         val noBottomNavigationIds = listOf(R.id.navigation_news_detail)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val shouldShowBottomNavigation = noBottomNavigationIds.contains(destination.id).not()
-            bottomNavigationView.isVisible = shouldShowBottomNavigation
+            navView.isVisible = shouldShowBottomNavigation
+            toolbar.isVisible = shouldShowBottomNavigation
         }
     }
 
     private fun setupNavController() = with(binding) {
         navController = findNavController(R.id.nav_host_fragment)
-        bottomNavigationView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_news,
+                R.id.navigation_favorites,
+                R.id.navigation_search
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
     fun showAddedToFavoritesDialog() {
